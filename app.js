7679a7772757efe048ccff0d50aadd250e1cb5c1
ghi3621 <ghi3621@gmail.com>
2024-01-24 17:52:24 +0900

    또 수정

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// import dotenv from "dotenv";
 import express from "express";
 import connect from "./schemas/index.js";
 import productsRouter from "./routes/products.router.js";
@@ -6,7 +5,6 @@ import productsRouter from "./routes/products.router.js";
 const app = express();
 const PORT = 3000;
 
-// dotenv.config();
 connect();
 
 // Express에서 req.body에 접근하여 body 데이터를 사용할 수 있도록 설정합니다.
@@ -19,7 +17,7 @@ router.get("/", (req, res) => {
   return res.json({ message: "Hi!" });
 });
 
-app.use("/api", productsRouter);
+app.use("/api", [router, productsRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
